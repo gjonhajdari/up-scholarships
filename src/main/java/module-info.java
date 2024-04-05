a7a18a2485b30200e9b4193bb7c1098f47d7278c
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2024-04-05 13:32:18 +0200

    Add MySQL depencency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module org.example.knk_projekti {
   requires javafx.fxml;
 
   requires org.kordamp.ikonli.javafx;
+  requires java.sql;
 
   opens org.example.knk_projekti to javafx.fxml;
   exports org.example.knk_projekti;
