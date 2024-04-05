a8327b6d1bb546356798e3c327f2b06f0e12d50c
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2024-04-05 22:49:48 +0200

    Add trigger to check voucher deadline before applying

--- src/main/db-structure.sql
+++ src/main/db-structure.sql
@@ -20,8 +20,7 @@ create table if not exists scholarships.voucher(
   title nvarchar(100) not null,
   amount decimal(10, 2) not null,
   deadline date not null,
-  primary key (voucher_id),
-  foreign key (student_id) references student(student_id)
+  primary key (voucher_id)
 );
 
 create table if not exists scholarships.application(
@@ -31,7 +30,7 @@ create table if not exists scholarships.application(
   application_date date not null,
   primary key (application_id),
   foreign key (student_id) references student(student_id),
-  foreign key (voucher_id) references scholarship(voucher_id)
+  foreign key (voucher_id) references voucher(voucher_id)
 );
 
 create table if not exists scholarships.awardees(
@@ -40,5 +39,34 @@ create table if not exists scholarships.awardees(
   award_date date not null,
   primary key (student_id, voucher_id),
   foreign key (student_id) references student(student_id),
-  foreign key (voucher_id) references scholarship(voucher_id)
+  foreign key (voucher_id) references voucher(voucher_id)
 );
+
+-- --------------------
+-- Triggers
+-- --------------------
+
+-- Checks if the deadline for a voucher has passed when applying
+
+DELIMITER //
+
+CREATE TRIGGER `scholarships`.`check_voucher_deadline`
+BEFORE INSERT ON `scholarships`.`application`
+FOR EACH ROW
+BEGIN
+    DECLARE deadline_passed BOOLEAN;
+
+    SELECT TRUE INTO deadline_passed
+    FROM `scholarships`.`voucher`
+    WHERE voucher_id = NEW.voucher_id
+    AND DATE(deadline) < DATE(NEW.application_date);
+
+    -- If the deadline has passed, raise an error
+    IF deadline_passed THEN
+        SIGNAL SQLSTATE "45000"
+        SET MESSAGE_TEXT = "Application deadline has passed. Cannot apply for scholarship.";
+    END IF;
+END;
+//
+
+DELIMITER ;
