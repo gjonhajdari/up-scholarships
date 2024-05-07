f452e2216a9e6213699eae294b81d01f5e27b49d
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2024-05-07 21:56:25 +0200

    Add PasswordHasher and Navigator classes

---
+++ src/main/java/service/Navigator.java
@@ -0,0 +1,37 @@
+package service;
+
+import javafx.event.Event;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Navigator {
+  public final static String HOME_PAGE = "home_page.fxml";
+  public final static String LOGIN_PAGE_USER = "login_page_user.fxml";
+  public final static String LOGIN_PAGE_ADMIN = "login_page_admin.fxml";
+  public final static String DASHBOARD_USER = "dashboard_user.fxml";
+  public final static String DASHBOARD_ADMIN = "dashboard_admin.fxml";
+
+  public static void navigate(Event event, String path) {
+    // Navigate -> event -> from current scene to new scene
+    Node eventNode = (Node) event.getSource();
+    Stage stage = (Stage) eventNode.getScene().getWindow();
+
+    navigate(stage, path);
+  }
+
+  public static void navigate(Stage stage, String path) {
+    FXMLLoader loader = new FXMLLoader(Navigator.class.getResource(path));
+
+    try {
+      Scene newScene = new Scene(loader.load());
+      stage.setScene(newScene);
+      stage.show();
+    } catch (IOException ioe) {
+
+    }
+  }
+}
