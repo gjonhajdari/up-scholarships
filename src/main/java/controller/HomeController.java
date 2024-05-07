6039c2cafc3abc7d852126b3cd8b858d3032494d
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2024-05-08 00:18:37 +0200

    Add home page controller

---
+++ src/main/java/controller/HomeController.java
@@ -0,0 +1,21 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class HomeController {
+  @FXML
+  private Button btnStudent;
+  @FXML
+  private Button btnAdmin;
+
+  @FXML
+  private void handleStudentClick() {
+
+  }
+
+  @FXML
+  private void handleAdminClick() {
+
+  }
+}
