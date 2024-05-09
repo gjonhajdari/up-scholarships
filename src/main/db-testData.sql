4927c5f60ba6085393b84fd5b5b8d004501b1ea7
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2024-05-09 19:36:29 +0200

    Update inserted student data with hashed password

--- src/main/db-testData.sql
+++ src/main/db-testData.sql
@@ -4,14 +4,14 @@
 
 use scholarships;
 
-insert into scholarships.student(student_id, first_name, last_name, email, password)
+insert into scholarships.student(student_id, first_name, last_name, email, salt, password)
 values
-("210756100052", "Gjon", "Hajdari", "gjon.hajdari@student.uni-pr.edu", "12345"),
-("220756100031", "Lorik", "Agaj", "lorik.agaj@student.uni-pr.edu", "12345"),
-("220756100004", "Dion", "Gashi", "dion.gashi@student.uni-pr.edu", "12345"),
-("220756100036", "Diell", "Doçi", "diell.doci@student.uni-pr.edu", "12345"),
-("220757100075", "Blerton", "Ismaili", "blerton.ismaili@student.uni-pr.edu", "12345"),
-("220756100109", "Gent", "Podvorica", "gent.podvorica@student.uni-pr.edu", "12345");
+("210756100052", "Gjon", "Hajdari", "gjon.hajdari@student.uni-pr.edu", "rSGGscpwXNz4ncOidA+kL+rQzMJWJlvQX9uuXEAfN2Y=", "7253474773637077584e7a346e634f6964412b6b4c2b72517a4d4a574a6c765158397575584541664e32593dac630711365a6543df6c074a164931f0ebe45a1b2b12d3c3199c6f13e1583b31"),
+("220756100031", "Lorik", "Agaj", "lorik.agaj@student.uni-pr.edu", "F9I8sC/AO20giA3x5D//PwBVTMUguqInuOhnyxjpzHA=", "4639493873432f414f3230676941337835442f2f50774256544d55677571496e754f686e79786a707a48413d96a36c7a14674093f73d91a290216ed03d85385c249d0ba057deb242aac4d5aa"),
+("220756100004", "Dion", "Gashi", "dion.gashi@student.uni-pr.edu", "7XfAI76chpfhW+upjHA3sGywgQc4trvXqnQL2dtOGGk=", "375866414937366368706668572b75706a484133734779776751633474727658716e514c3264744f47476b3d99964f81b61f431598175ecbc3dd8b1867e4a2f910c11afa032331e48f283eff"),
+("220756100036", "Diell", "Doçi", "diell.doci@student.uni-pr.edu", "w9QfuYhboyuQwEHnIVH09HZjXMDzrOJllzZRkz167Rw=", "77395166755968626f7975517745486e4956483039485a6a584d447a724f4a6c6c7a5a526b7a31363752773d705223584c6994129e588033b13a1cbeff977f2769f35ba60b669d94a984c6ea"),
+("220757100075", "Blerton", "Ismaili", "blerton.ismaili@student.uni-pr.edu", "/X8VPAnu3Z64VR7+vKyllirYdT8/tCXIkVwTGyXOORQ=", "2f58385650416e75335a36345652372b764b796c6c6972596454382f744358496b5677544779584f4f52513da18e804b01ddc6d3a2e2dc0735083632c03b99aee03bb1afe8d373cb5116fd1e"),
+("220756100109", "Gent", "Podvorica", "gent.podvorica@student.uni-pr.edu", "w+5S+8jXsvNBSbHzfOfcfN4UAGbHDN+gYBLZ2qbzhYU=", "772b35532b386a5873764e425362487a664f6663664e345541476248444e2b6759424c5a3271627a6859553dd36d42d36028078067df9a7982c30cc17d28c9e980b7554e2bb2aa00ed3ef06b");
 
 insert into scholarships.voucher(title, amount, deadline)
 values
