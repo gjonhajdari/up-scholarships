93e45b6d63034822e0b55b22fc576e9fe015bc98
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2024-05-09 19:35:19 +0200

    Rename fxml student pages

--- src/main/java/service/Navigator.java
+++ src/main/java/service/Navigator.java
@@ -10,10 +10,12 @@ import java.io.IOException;
 
 public class Navigator {
   public final static String HOME_PAGE = "home_page.fxml";
-  public final static String LOGIN_PAGE_STUDENT = "login_page_user.fxml";
+  public final static String LOGIN_PAGE_STUDENT = "login_page_student.fxml";
+  public final static String DASHBOARD_STUDENT = "dashboard_page_student.fxml";
+  public final static String PROFILE_STUDENT = "profile_page_student.fxml";
   public final static String LOGIN_PAGE_ADMIN = "login_page_admin.fxml";
-  public final static String DASHBOARD_STUDENT = "dashboard_user.fxml";
-  public final static String DASHBOARD_ADMIN = "dashboard_admin.fxml";
+  public final static String DASHBOARD_ADMIN = "dashboard_page_admin.fxml";
+  public final static String CREATE_VOUCHER = "create_voucher_page.fxml";
 
   public static void navigate(Event event, String path) {
     // Navigate -> event -> from current scene to new scene
