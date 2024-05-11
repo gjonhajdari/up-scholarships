425d014aaec9e6ebe34e131c21c1b107df961be4
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2024-05-11 17:36:36 +0200

    Add icons to admin sidebar

--- src/main/java/controller/CreateVoucherController.java
+++ src/main/java/controller/CreateVoucherController.java
@@ -1,11 +1,31 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import service.AdminSession;
 import service.Navigator;
 
 public class CreateVoucherController {
+    @FXML
+    private TextField txtTitle;
+    @FXML
+    private TextField txtAmount;
+    @FXML
+    private DatePicker dtpDeadline;
+    @FXML
+    private ComboBox<String> cmbCategory;
+    @FXML
+    private TextArea txtDescription;
+
+    @FXML
+    private void handleCreateClick(MouseEvent me) {
+
+    }
+
     @FXML
     private void handleDashboardClick(MouseEvent me) { Navigator.navigate(me, Navigator.DASHBOARD_ADMIN); }
     @FXML
