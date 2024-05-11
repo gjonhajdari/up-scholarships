4f84c150d0c5eff9064b518367d54728f25e1fe1
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2024-05-12 01:24:26 +0200

    Add table to display vouchers

--- src/main/java/controller/DashboardStudentController.java
+++ src/main/java/controller/DashboardStudentController.java
@@ -1,18 +1,44 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
+import model.Voucher;
 import service.Navigator;
 import service.UserSession;
+import service.VoucherService;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public class DashboardStudentController {
     @FXML
     private Text txtWelcomeMessage;
+    @FXML
+    private TableView<Voucher> tblDashboardStudent;
+    @FXML
+    private TableColumn<Voucher, String> colTitle;
+    @FXML
+    private TableColumn<Voucher, Float> colAmount;
+    @FXML
+    private TableColumn<Voucher, LocalDate> colDeadline;
 
     public void initialize() {
         String firstName = UserSession.getInstance(null).getFirstName();
         txtWelcomeMessage.setText("Welcome back, " + firstName);
+
+        colTitle.setCellValueFactory(new PropertyValueFactory<>("title"));
+        colAmount.setCellValueFactory(new PropertyValueFactory<>("amount"));
+        colDeadline.setCellValueFactory(new PropertyValueFactory<>("deadline"));
+
+        List<Voucher> vouchers = VoucherService.getAllVouchers();
+        ObservableList<Voucher> observableVouchers = FXCollections.observableArrayList(vouchers);
+        tblDashboardStudent.setItems(observableVouchers);
     }
 
     @FXML
