f78115e461b430d3f4a0e9c9fc0bdc3706472564
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2024-05-12 20:36:10 +0200

    Add admin table with all vouchers

--- src/main/java/controller/VouchersAdminController.java
+++ src/main/java/controller/VouchersAdminController.java
@@ -3,7 +3,6 @@ package controller;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
@@ -18,19 +17,8 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class VouchersAdminController {
-
     @FXML
-    private Text txtVoucherId;
-    @FXML
-    private Text txtTitle;
-    @FXML
-    private Text txtAmount;
-    @FXML
-    private Text txtDeadline;
-    @FXML
-    private Text txtDescription;
-    @FXML
-    private TableView<Voucher> tblDashboardStudent;
+    private TableView<Voucher> tblVouchersAdmin;
     @FXML
     private TableColumn<Voucher, String> colTitle;
     @FXML
@@ -38,48 +26,35 @@ public class VouchersAdminController {
     @FXML
     private TableColumn<Voucher, LocalDate> colDeadline;
 
-    private int voucherId;
-
-    public void initData(int id) {
-        voucherId = id;
-        txtVoucherId.setText("Voucher #" + String.valueOf(voucherId));
-
-        Voucher voucher = VoucherService.getVoucherById(id);
-
-        txtTitle.setText(voucher.getTitle());
-        txtAmount.setText(Formatter.formatCurrency(voucher.getAmount()) + " EUR");
-        txtDeadline.setText(Formatter.formatDate(voucher.getDeadline()));
-        txtDescription.setText(voucher.getDescription());
-    }
     public void initialize() {
-
-
         colTitle.setCellValueFactory(new PropertyValueFactory<>("title"));
         colAmount.setCellValueFactory(new PropertyValueFactory<>("amount"));
         colDeadline.setCellValueFactory(new PropertyValueFactory<>("deadline"));
 
         List<Voucher> vouchers = VoucherService.getAllVouchers();
         ObservableList<Voucher> observableVouchers = FXCollections.observableArrayList(vouchers);
-        tblDashboardStudent.setItems(observableVouchers);
+        tblVouchersAdmin.setItems(observableVouchers);
 
         // Listening for a click on a row to navigate to the dynamic voucher page
-        tblDashboardStudent.setRowFactory(tv -> {
+        tblVouchersAdmin.setRowFactory(tv -> {
             TableRow<Voucher> row = new TableRow<>();
 
             row.setOnMouseClicked(event -> {
                 if (!row.isEmpty() && event.getButton() == MouseButton.PRIMARY) {
                     Voucher clickedRow = row.getItem();
-                    Navigator.navigate(event, Navigator.APPLICATIONS_PER_VOUCHER, clickedRow.getId());
+                    Navigator.navigate(event, Navigator.APPLICANTS_PAGE, clickedRow.getId());
                 }
             });
 
             return row;
         });
     }
+
     @FXML
     private void handleAddNewClick(MouseEvent me){
         Navigator.navigate(me,Navigator.CREATE_VOUCHER);
     }
+    
     @FXML
     private void handleDashboardClick(MouseEvent me) { Navigator.navigate(me, Navigator.DASHBOARD_ADMIN); }
     @FXML
