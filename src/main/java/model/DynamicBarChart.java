127ddcbf50caa0cfcc46cdb0aa7aa0d83b772d16
gjonhajdari <gjon.hajdari@student.uni-pr.edu>
2024-05-17 21:43:41 +0200

    Remove value labels from `DynamicBarChart`

--- src/main/java/model/DynamicBarChart.java
+++ src/main/java/model/DynamicBarChart.java
@@ -3,12 +3,10 @@ package model;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import javafx.scene.text.Text;
 
 import java.util.List;
 import java.util.Map;
@@ -16,12 +14,10 @@ import java.util.stream.Collectors;
 
 public class DynamicBarChart extends BarChart<String, Number> {
 
-    private Group parentGroup;
     private ObservableList<VoucherApplied> data = FXCollections.observableArrayList();
 
     public DynamicBarChart() {
         super(new CategoryAxis(), new NumberAxis());
-        parentGroup = new Group();
         setLabels();
     }
 
@@ -47,43 +43,9 @@ public class DynamicBarChart extends BarChart<String, Number> {
         XYChart.Series<String, Number> series = new XYChart.Series<>();
         statusCounts.forEach((status, count) -> {
             XYChart.Data<String, Number> data = new XYChart.Data<>(status, count);
-            data.nodeProperty().addListener((observable, oldValue, newValue) -> {
-                if (newValue != null) {
-                    displayLabelForData(data);
-                }
-            });
             series.getData().add(data);
         });
 
         return series;
-    }
-
-    private void displayLabelForData(XYChart.Data<String, Number> data) {
-        final Node node = data.getNode();
-        final Text dataText = new Text(data.getYValue() + "");
-
-        data.extraValueProperty().setValue(dataText);
-
-        node.parentProperty().addListener((ov, oldParent, parent) -> {
-            Group parentGroup = (Group) parent;
-            if (parentGroup != null) {
-                parentGroup.getChildren().add(dataText);
-            }
-        });
-
-        node.boundsInParentProperty().addListener((ov, oldBounds, bounds) -> {
-            dataText.setLayoutX(Math.round(bounds.getMinX() + bounds.getWidth() / 2 - dataText.prefWidth(-1) / 2));
-            dataText.setLayoutY(Math.round(bounds.getMinY() - dataText.prefHeight(-1) * 0.5));
-        });
-
-        // Remove the old Text node when the data changes
-        data.YValueProperty().addListener((ov, oldVal, newVal) -> {
-            Group parentGroup = (Group) node.getParent();
-            if (parentGroup != null) {
-                Text oldDataText = (Text) data.extraValueProperty().getValue();
-                parentGroup.getChildren().remove(oldDataText);
-                dataText.setText(newVal + "");
-            }
-        });
     }
 }
