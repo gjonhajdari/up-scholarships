b4de530cdf08a123d52cce7da78ec284897a904f
gjonhajdari <gjon.hajdari@student.uni-pr.edu>
2024-05-19 18:54:31 +0200

    Refactor `DynamicBarChart` to have a tab width of 2

--- src/main/java/model/DynamicBarChart.java
+++ src/main/java/model/DynamicBarChart.java
@@ -13,39 +13,38 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class DynamicBarChart extends BarChart<String, Number> {
+  private ObservableList<VoucherApplied> data = FXCollections.observableArrayList();
 
-    private ObservableList<VoucherApplied> data = FXCollections.observableArrayList();
+  public DynamicBarChart() {
+    super(new CategoryAxis(), new NumberAxis());
+    setLabels();
+  }
 
-    public DynamicBarChart() {
-        super(new CategoryAxis(), new NumberAxis());
-        setLabels();
-    }
+  private void setLabels() {
+    ((CategoryAxis)getXAxis()).setLabel("Status");
+    ((NumberAxis)getYAxis()).setLabel("Number");
+  }
 
-    private void setLabels() {
-        ((CategoryAxis)getXAxis()).setLabel("Status");
-        ((NumberAxis)getYAxis()).setLabel("Number");
-    }
+  public void updateData(List<VoucherApplied> voucherAppliedList) {
+    data.setAll(voucherAppliedList);
+    loadData();
+  }
 
-    public void updateData(List<VoucherApplied> voucherAppliedList) {
-        data.setAll(voucherAppliedList);
-        loadData();
-    }
+  private void loadData() {
+    getData().clear();
+    getData().add(generateSeriesFromData());
+  }
 
-    private void loadData() {
-        getData().clear();
-        getData().add(generateSeriesFromData());
-    }
+  private XYChart.Series<String, Number> generateSeriesFromData() {
+    Map<String, Long> statusCounts = data.stream()
+            .collect(Collectors.groupingBy(VoucherApplied::getStatus, Collectors.counting()));
 
-    private XYChart.Series<String, Number> generateSeriesFromData() {
-        Map<String, Long> statusCounts = data.stream()
-                .collect(Collectors.groupingBy(VoucherApplied::getStatus, Collectors.counting()));
+    XYChart.Series<String, Number> series = new XYChart.Series<>();
+    statusCounts.forEach((status, count) -> {
+      XYChart.Data<String, Number> data = new XYChart.Data<>(status, count);
+      series.getData().add(data);
+    });
 
-        XYChart.Series<String, Number> series = new XYChart.Series<>();
-        statusCounts.forEach((status, count) -> {
-            XYChart.Data<String, Number> data = new XYChart.Data<>(status, count);
-            series.getData().add(data);
-        });
-
-        return series;
-    }
+    return series;
+  }
 }
