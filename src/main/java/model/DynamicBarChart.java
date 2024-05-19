5609e31afdac54c00c52e8ff840f8a34877da2b7
gjonhajdari <gjon.hajdari@student.uni-pr.edu>
2024-05-19 19:39:25 +0200

    Add `DynamicBarChart` bar coloring depending on status

--- src/main/java/model/DynamicBarChart.java
+++ src/main/java/model/DynamicBarChart.java
@@ -1,38 +1,32 @@
 package model;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.scene.Group;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class DynamicBarChart extends BarChart<String, Number> {
-  private ObservableList<VoucherApplied> data = FXCollections.observableArrayList();
+public class DynamicBarChart extends DynamicChart {
+  private BarChart<String, Number> barChart;
 
   public DynamicBarChart() {
-    super(new CategoryAxis(), new NumberAxis());
+    barChart = new BarChart<>(new CategoryAxis(), new NumberAxis());
+    barChart.setLegendVisible(false);
+
     setLabels();
   }
 
   private void setLabels() {
-    ((CategoryAxis)getXAxis()).setLabel("Status");
-    ((NumberAxis)getYAxis()).setLabel("Number");
+    ((CategoryAxis)barChart.getXAxis()).setLabel("Status");
+    ((NumberAxis)barChart.getYAxis()).setLabel("Number");
   }
 
-  public void updateData(List<VoucherApplied> voucherAppliedList) {
-    data.setAll(voucherAppliedList);
-    loadData();
-  }
-
-  private void loadData() {
-    getData().clear();
-    getData().add(generateSeriesFromData());
+  @Override
+  protected void loadData() {
+    barChart.getData().clear();
+    barChart.getData().add(generateSeriesFromData());
   }
 
   private XYChart.Series<String, Number> generateSeriesFromData() {
@@ -40,11 +34,32 @@ public class DynamicBarChart extends BarChart<String, Number> {
             .collect(Collectors.groupingBy(VoucherApplied::getStatus, Collectors.counting()));
 
     XYChart.Series<String, Number> series = new XYChart.Series<>();
+
     statusCounts.forEach((status, count) -> {
       XYChart.Data<String, Number> data = new XYChart.Data<>(status, count);
       series.getData().add(data);
+      // Set style class depending on application status
+      data.nodeProperty().addListener((observable, oldValue, newValue) -> {
+        if (newValue != null) {
+          switch (status) {
+            case "PENDING":
+              newValue.setStyle("-fx-bar-fill: #94a3b8;");
+              break;
+            case "APPROVED":
+              newValue.setStyle("-fx-bar-fill: #4ade80;");
+              break;
+            case "REJECTED":
+              newValue.setStyle("-fx-bar-fill: #f87171;");
+              break;
+          }
+      }
+      });
     });
 
     return series;
+  }
+
+  public BarChart<String, Number> getChart() {
+    return barChart;
   }
 }
