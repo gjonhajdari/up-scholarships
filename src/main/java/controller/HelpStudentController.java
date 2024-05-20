33e58f0a7b8e44b8abe887ab2da11a111b29c28c
gjonhajdari <gjon.hajdari@student.uni-pr.edu>
2024-05-20 23:12:42 +0200

    Remove comments

--- src/main/java/controller/HelpStudentController.java
+++ src/main/java/controller/HelpStudentController.java
@@ -7,25 +7,21 @@ import service.UserSession;
 
 public class HelpStudentController {
   @FXML
-  private void handleDashboardClick(MouseEvent me) { Navigator.navigate( me, Navigator.DASHBOARD_STUDENT); }
+  private void handleKosovoGovernmentButton(MouseEvent me) { Navigator.navigate(me, Navigator.KosovoGovernment_EXTERNAL_PAGE); }
   @FXML
-  private void handleProfileClick(MouseEvent me) { Navigator.navigate( me, Navigator.PROFILE_STUDENT); }
+  private void handleMASHTExternalButton(MouseEvent me) { Navigator.navigate(me, Navigator.MASHT_EXTERNAL_PAGE); }
   @FXML
-  private void handleAppliedClick(MouseEvent me) { Navigator.navigate( me, Navigator.APPLIED_STUDENT); }
-//  @FXML
-//  private void handleDashboardButtonClick(MouseEvent me) { Navigator.navigate(me,Navigator.DASHBOARD_STUDENT); }
-//  @FXML
-//  private void handleAppliedButtonClick(MouseEvent me) { Navigator.navigate(me, Navigator.APPLIED_STUDENT); }
-//  @FXML
-//  private void handleProfileButtonClick(MouseEvent me) {Navigator.navigate(me,Navigator.PROFILE_STUDENT);}
+  private void handleMFPTButtonClick(MouseEvent me) { Navigator.navigate(me, Navigator.MFPT_EXTERNAL_PAGE); }
   @FXML
-  private void handleKosovoGovernmentButton(MouseEvent me) {Navigator.navigate(me,Navigator.KosovoGovernment_EXTERNAL_PAGE);}
+  private void handleUPExternalButtonClick(MouseEvent me) { Navigator.navigate(me, Navigator.UP_EXTERNAL_PAGE); }
+
+
   @FXML
-  private void handleMASHTExternalButton(MouseEvent me) {Navigator.navigate(me,Navigator.MASHT_EXTERNAL_PAGE);}
+  private void handleDashboardClick(MouseEvent me) { Navigator.navigate(me, Navigator.DASHBOARD_STUDENT); }
   @FXML
-  private void handleMFPTButtonClick(MouseEvent me) {Navigator.navigate(me,Navigator.MFPT_EXTERNAL_PAGE);}
+  private void handleProfileClick(MouseEvent me) { Navigator.navigate(me, Navigator.PROFILE_STUDENT); }
   @FXML
-  private void handleUPExternalButtonClick(MouseEvent me) {Navigator.navigate(me,Navigator.UP_EXTERNAL_PAGE);}
+  private void handleAppliedClick(MouseEvent me) { Navigator.navigate(me, Navigator.APPLIED_STUDENT); }
   @FXML
   private void handleLogoutClick(MouseEvent me) {
       UserSession.clearUserSession();
