376067886aacdd91fdaf293de997b8b603a5fc72
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2024-09-20 21:35:30 +0200

    Update README database instructions

--- README.md
+++ README.md
@@ -16,12 +16,21 @@ Open your terminal and access the MySQL CLI to run the database scripts
 
 ```bash
 mysql -u root -p < src/main/db-structure.sql
-mysql -u root -p < src/main/db-testData.sql
 ```
 
 Replace `root` with your MySQL username if you have it configured differently. You will be prompted to enter your password (if you don't have one just hit enter).
 
-Alternatively you can open the scripts and run them in your favorite MySQL GUI.
+#### Add all student accounts
+
+You can add all the test accounts by running the `Migration.java` file. It creates accounts using the student data from the `students.csv` file.
+
+Run the `db-testData.sql` script to add the test data to the database.
+
+```bash
+mysql -u root -p < src/main/db-testData.sql
+```
+
+Alternatively you can open the sql scripts and run them in your favorite MySQL GUI.
 
 ### Configure Database Connection
 
@@ -37,13 +46,6 @@ public class ConnectionUtil {
   // ...
 }
 ```
-
-### Add all student accounts
-
-The `db-testData.sql` script adds a few student accounts to the database. You can add the rest by running the `Migration.java` file. It creates accounts using the student data from the `students.csv` file.
-
-After that you can uncomment the remaining `insert_application_random()` calls in the `db-testData.sql` script and run it again.
-
 
 ## Screenshots
 
--- src/db-testData.sql
+++ src/db-testData.sql
@@ -4,14 +4,14 @@
 
 use scholarships;
 
-insert into scholarships.student(student_id, first_name, last_name, email, salt, password)
-values
-("210756100052", "Gjon", "Hajdari", "gjon.hajdari@student.uni-pr.edu", "rSGGscpwXNz4ncOidA+kL+rQzMJWJlvQX9uuXEAfN2Y=", "7253474773637077584e7a346e634f6964412b6b4c2b72517a4d4a574a6c765158397575584541664e32593dac630711365a6543df6c074a164931f0ebe45a1b2b12d3c3199c6f13e1583b31"),
-("220756100031", "Lorik", "Agaj", "lorik.agaj@student.uni-pr.edu", "F9I8sC/AO20giA3x5D//PwBVTMUguqInuOhnyxjpzHA=", "4639493873432f414f3230676941337835442f2f50774256544d55677571496e754f686e79786a707a48413d96a36c7a14674093f73d91a290216ed03d85385c249d0ba057deb242aac4d5aa"),
-("220756100004", "Dion", "Gashi", "dion.gashi@student.uni-pr.edu", "7XfAI76chpfhW+upjHA3sGywgQc4trvXqnQL2dtOGGk=", "375866414937366368706668572b75706a484133734779776751633474727658716e514c3264744f47476b3d99964f81b61f431598175ecbc3dd8b1867e4a2f910c11afa032331e48f283eff"),
-("220756100036", "Diell", "Doçi", "diell.doci@student.uni-pr.edu", "w9QfuYhboyuQwEHnIVH09HZjXMDzrOJllzZRkz167Rw=", "77395166755968626f7975517745486e4956483039485a6a584d447a724f4a6c6c7a5a526b7a31363752773d705223584c6994129e588033b13a1cbeff977f2769f35ba60b669d94a984c6ea"),
-("220757100075", "Blerton", "Ismaili", "blerton.ismaili@student.uni-pr.edu", "/X8VPAnu3Z64VR7+vKyllirYdT8/tCXIkVwTGyXOORQ=", "2f58385650416e75335a36345652372b764b796c6c6972596454382f744358496b5677544779584f4f52513da18e804b01ddc6d3a2e2dc0735083632c03b99aee03bb1afe8d373cb5116fd1e"),
-("220756100109", "Gent", "Podvorica", "gent.podvorica@student.uni-pr.edu", "w+5S+8jXsvNBSbHzfOfcfN4UAGbHDN+gYBLZ2qbzhYU=", "772b35532b386a5873764e425362487a664f6663664e345541476248444e2b6759424c5a3271627a6859553dd36d42d36028078067df9a7982c30cc17d28c9e980b7554e2bb2aa00ed3ef06b");
+-- insert into scholarships.student(student_id, first_name, last_name, email, salt, password)
+-- values
+-- ("210756100052", "Gjon", "Hajdari", "gjon.hajdari@student.uni-pr.edu", "rSGGscpwXNz4ncOidA+kL+rQzMJWJlvQX9uuXEAfN2Y=", "7253474773637077584e7a346e634f6964412b6b4c2b72517a4d4a574a6c765158397575584541664e32593dac630711365a6543df6c074a164931f0ebe45a1b2b12d3c3199c6f13e1583b31"),
+-- ("220756100031", "Lorik", "Agaj", "lorik.agaj@student.uni-pr.edu", "F9I8sC/AO20giA3x5D//PwBVTMUguqInuOhnyxjpzHA=", "4639493873432f414f3230676941337835442f2f50774256544d55677571496e754f686e79786a707a48413d96a36c7a14674093f73d91a290216ed03d85385c249d0ba057deb242aac4d5aa"),
+-- ("220756100004", "Dion", "Gashi", "dion.gashi@student.uni-pr.edu", "7XfAI76chpfhW+upjHA3sGywgQc4trvXqnQL2dtOGGk=", "375866414937366368706668572b75706a484133734779776751633474727658716e514c3264744f47476b3d99964f81b61f431598175ecbc3dd8b1867e4a2f910c11afa032331e48f283eff"),
+-- ("220756100036", "Diell", "Doçi", "diell.doci@student.uni-pr.edu", "w9QfuYhboyuQwEHnIVH09HZjXMDzrOJllzZRkz167Rw=", "77395166755968626f7975517745486e4956483039485a6a584d447a724f4a6c6c7a5a526b7a31363752773d705223584c6994129e588033b13a1cbeff977f2769f35ba60b669d94a984c6ea"),
+-- ("220757100075", "Blerton", "Ismaili", "blerton.ismaili@student.uni-pr.edu", "/X8VPAnu3Z64VR7+vKyllirYdT8/tCXIkVwTGyXOORQ=", "2f58385650416e75335a36345652372b764b796c6c6972596454382f744358496b5677544779584f4f52513da18e804b01ddc6d3a2e2dc0735083632c03b99aee03bb1afe8d373cb5116fd1e"),
+-- ("220756100109", "Gent", "Podvorica", "gent.podvorica@student.uni-pr.edu", "w+5S+8jXsvNBSbHzfOfcfN4UAGbHDN+gYBLZ2qbzhYU=", "772b35532b386a5873764e425362487a664f6663664e345541476248444e2b6759424c5a3271627a6859553dd36d42d36028078067df9a7982c30cc17d28c9e980b7554e2bb2aa00ed3ef06b");
 
 -- insert into scholarships.voucher(title, amount, category, description, deadline)
 -- values
@@ -111,96 +111,96 @@ CALL insert_application_random("220756100031", 17);
 CALL insert_application_random("220756100004", 18);
 CALL insert_application_random("210756100052", 19);
 CALL insert_application_random("220756100031", 20);
--- CALL insert_application_random('220756100063', 1);
--- CALL insert_application_random('220756100075', 2);
--- CALL insert_application_random('220756100072', 3);
--- CALL insert_application_random('220756100084', 4);
--- CALL insert_application_random('220756100052', 5);
--- CALL insert_application_random('220756100058', 6);
--- CALL insert_application_random('220756100035', 7);
--- CALL insert_application_random('220756100026', 8);
--- CALL insert_application_random('220756100037', 9);
--- CALL insert_application_random('220756100092', 10);
--- CALL insert_application_random('220756100088', 11);
--- CALL insert_application_random('220756100067', 12);
--- CALL insert_application_random('220756100135', 13);
--- CALL insert_application_random('220756100056', 14);
--- CALL insert_application_random('220756100057', 15);
--- CALL insert_application_random('220756100025', 16);
--- CALL insert_application_random('220756100096', 17);
--- CALL insert_application_random('220756100051', 18);
--- CALL insert_application_random('220756100010', 19);
--- CALL insert_application_random('220756100079', 20);
--- CALL insert_application_random('220756100114', 21);
--- CALL insert_application_random('220756100130', 22);
--- CALL insert_application_random('220756100027', 23);
--- CALL insert_application_random('220756100022', 24);
--- CALL insert_application_random('220756100078', 25);
--- CALL insert_application_random('220756100039', 26);
--- CALL insert_application_random('220756100018', 27);
--- CALL insert_application_random('220756100117', 28);
--- CALL insert_application_random('220756100099', 29);
--- CALL insert_application_random('220756100016', 30);
--- CALL insert_application_random('220756100076', 1);
--- CALL insert_application_random('220757100068', 2);
--- CALL insert_application_random('220756100032', 3);
--- CALL insert_application_random('220757100075', 4);
--- CALL insert_application_random('220756100034', 5);
--- CALL insert_application_random('220756100021', 6);
--- CALL insert_application_random('220756100017', 7);
--- CALL insert_application_random('220756100060', 8);
--- CALL insert_application_random('220756100048', 9);
--- CALL insert_application_random('220756100113', 10);
--- CALL insert_application_random('220756100070', 11);
--- CALL insert_application_random('220756100069', 12);
--- CALL insert_application_random('220756100036', 13);
--- CALL insert_application_random('220756100004', 14);
--- CALL insert_application_random('220756100083', 15);
--- CALL insert_application_random('220718100036', 16);
--- CALL insert_application_random('220756100105', 17);
--- CALL insert_application_random('220756100043', 18);
--- CALL insert_application_random('220756100009', 19);
--- CALL insert_application_random('220756100087', 20);
--- CALL insert_application_random('220756100125', 21);
--- CALL insert_application_random('220756100002', 22);
--- CALL insert_application_random('220756100042', 23);
--- CALL insert_application_random('220756100134', 24);
--- CALL insert_application_random('220756100077', 25);
--- CALL insert_application_random('220756100045', 26);
--- CALL insert_application_random('220756100090', 27);
--- CALL insert_application_random('220756100029', 28);
--- CALL insert_application_random('220756100110', 29);
--- CALL insert_application_random('220758100003', 30);
--- CALL insert_application_random('220756100012', 1);
--- CALL insert_application_random('220756100131', 2);
--- CALL insert_application_random('220756100015', 3);
--- CALL insert_application_random('220757100055', 4);
--- CALL insert_application_random('220756100005', 5);
--- CALL insert_application_random('220756100108', 6);
--- CALL insert_application_random('220756100106', 7);
--- CALL insert_application_random('220756100061', 8);
--- CALL insert_application_random('210756100030', 9);
--- CALL insert_application_random('220756100062', 10);
--- CALL insert_application_random('220756100123', 11);
--- CALL insert_application_random('220756100014', 12);
--- CALL insert_application_random('220756100023', 13);
--- CALL insert_application_random('220756100081', 14);
--- CALL insert_application_random('220756100071', 15);
--- CALL insert_application_random('220756100001', 16);
--- CALL insert_application_random('220756100126', 17);
--- CALL insert_application_random('220756100085', 18);
--- CALL insert_application_random('220756100109', 19);
--- CALL insert_application_random('220756100119', 20);
--- CALL insert_application_random('220756100065', 21);
--- CALL insert_application_random('220756100007', 22);
--- CALL insert_application_random('220758100024', 23);
--- CALL insert_application_random('210756100052', 24);
--- CALL insert_application_random('220757100064', 25);
--- CALL insert_application_random('220756100086', 26);
--- CALL insert_application_random('220756100066', 27);
--- CALL insert_application_random('220756100055', 28);
--- CALL insert_application_random('220756100121', 29);
--- CALL insert_application_random('220756100128', 30);
+CALL insert_application_random('220756100063', 1);
+CALL insert_application_random('220756100075', 2);
+CALL insert_application_random('220756100072', 3);
+CALL insert_application_random('220756100084', 4);
+CALL insert_application_random('220756100052', 5);
+CALL insert_application_random('220756100058', 6);
+CALL insert_application_random('220756100035', 7);
+CALL insert_application_random('220756100026', 8);
+CALL insert_application_random('220756100037', 9);
+CALL insert_application_random('220756100092', 10);
+CALL insert_application_random('220756100088', 11);
+CALL insert_application_random('220756100067', 12);
+CALL insert_application_random('220756100135', 13);
+CALL insert_application_random('220756100056', 14);
+CALL insert_application_random('220756100057', 15);
+CALL insert_application_random('220756100025', 16);
+CALL insert_application_random('220756100096', 17);
+CALL insert_application_random('220756100051', 18);
+CALL insert_application_random('220756100010', 19);
+CALL insert_application_random('220756100079', 20);
+CALL insert_application_random('220756100114', 21);
+CALL insert_application_random('220756100130', 22);
+CALL insert_application_random('220756100027', 23);
+CALL insert_application_random('220756100022', 24);
+CALL insert_application_random('220756100078', 25);
+CALL insert_application_random('220756100039', 26);
+CALL insert_application_random('220756100018', 27);
+CALL insert_application_random('220756100117', 28);
+CALL insert_application_random('220756100099', 29);
+CALL insert_application_random('220756100016', 30);
+CALL insert_application_random('220756100076', 1);
+CALL insert_application_random('220757100068', 2);
+CALL insert_application_random('220756100032', 3);
+CALL insert_application_random('220757100075', 4);
+CALL insert_application_random('220756100034', 5);
+CALL insert_application_random('220756100021', 6);
+CALL insert_application_random('220756100017', 7);
+CALL insert_application_random('220756100060', 8);
+CALL insert_application_random('220756100048', 9);
+CALL insert_application_random('220756100113', 10);
+CALL insert_application_random('220756100070', 11);
+CALL insert_application_random('220756100069', 12);
+CALL insert_application_random('220756100036', 13);
+CALL insert_application_random('220756100004', 14);
+CALL insert_application_random('220756100083', 15);
+CALL insert_application_random('220718100036', 16);
+CALL insert_application_random('220756100105', 17);
+CALL insert_application_random('220756100043', 18);
+CALL insert_application_random('220756100009', 19);
+CALL insert_application_random('220756100087', 20);
+CALL insert_application_random('220756100125', 21);
+CALL insert_application_random('220756100002', 22);
+CALL insert_application_random('220756100042', 23);
+CALL insert_application_random('220756100134', 24);
+CALL insert_application_random('220756100077', 25);
+CALL insert_application_random('220756100045', 26);
+CALL insert_application_random('220756100090', 27);
+CALL insert_application_random('220756100029', 28);
+CALL insert_application_random('220756100110', 29);
+CALL insert_application_random('220758100003', 30);
+CALL insert_application_random('220756100012', 1);
+CALL insert_application_random('220756100131', 2);
+CALL insert_application_random('220756100015', 3);
+CALL insert_application_random('220757100055', 4);
+CALL insert_application_random('220756100005', 5);
+CALL insert_application_random('220756100108', 6);
+CALL insert_application_random('220756100106', 7);
+CALL insert_application_random('220756100061', 8);
+CALL insert_application_random('210756100030', 9);
+CALL insert_application_random('220756100062', 10);
+CALL insert_application_random('220756100123', 11);
+CALL insert_application_random('220756100014', 12);
+CALL insert_application_random('220756100023', 13);
+CALL insert_application_random('220756100081', 14);
+CALL insert_application_random('220756100071', 15);
+CALL insert_application_random('220756100001', 16);
+CALL insert_application_random('220756100126', 17);
+CALL insert_application_random('220756100085', 18);
+CALL insert_application_random('220756100109', 19);
+CALL insert_application_random('220756100119', 20);
+CALL insert_application_random('220756100065', 21);
+CALL insert_application_random('220756100007', 22);
+CALL insert_application_random('220758100024', 23);
+CALL insert_application_random('210756100052', 24);
+CALL insert_application_random('220757100064', 25);
+CALL insert_application_random('220756100086', 26);
+CALL insert_application_random('220756100066', 27);
+CALL insert_application_random('220756100055', 28);
+CALL insert_application_random('220756100121', 29);
+CALL insert_application_random('220756100128', 30);
 
 
 insert into scholarships.admin(username, salt, password)
